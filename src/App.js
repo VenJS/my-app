8eef458bb4ea92ac5e6c217ed7ad2ad29acbe4fd
Venci <venci34@gmail.com>
2022-01-11 23:18:30 +0200

    dfgfdgfd

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      search: "",
     };
   }
 
@@ -20,6 +21,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={(e) => {
+            this.setState({ searchField: e.target.value });
+          }}
+        ></input>
         <CardList monsters={this.state.monsters}></CardList>
       </div>
     );
